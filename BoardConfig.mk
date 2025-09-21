e7382fe30e56089e031de4eda4bd592f444fd69e
dodyirawan85 <dev.irawans@gmail.com>
2025-09-21 22:28:29 +0700

    fogos: Setup our OTA link
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ TARGET_TAP_TO_WAKE_NODE := /sys/class/touchscreen/primary/gesture
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
